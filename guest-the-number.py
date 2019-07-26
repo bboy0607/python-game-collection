6fa8c5ac32b7ea9f51ec308aa1f56993818dd04f
HankTsai <bboy0607@gmail.com>
2019-07-26 08:04:48 +0000

    update

--- guest-the-number.py
+++ guest-the-number.py
@@ -1,17 +1,20 @@
 import random
-
+  
 answer = random.randint(1,101)
+num_min = 1
+num_max = 100
 count = 0
 
 while True :
     count += 1
-    num = int(input('請輸入 1 ~ 100 間的數值: '))
+    print('請輸入 %d ~ %d 間的數值' %(num_min, num_max))
+    num = int(input('輸入: ' ))
     if num < answer:
         print('再大一點')
-        
+        num_min = num
     elif num > answer:
         print('再小一點')
-
+        num_max = num
     else:
-        print('恭喜你答對啦!答案是 %d，你猜了 %d 次' %(answer, count))
+        print('恭喜你答對啦!答案是 %d，你總共猜了 %d 次。' %(answer, count))
         break
